a0a2cafccd5f844f8470f6610b72692d711f0e99
janivkk <jasjasiaczek02@gmail.com>
2023-05-17 23:19:18 +0100

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,7 +1,6 @@
 import splitfolders
 
-#C:\Users\janek\__haemorrhage_project__\__datasets__\x_lrg_formatted\all
-
-#print("Splitting folder {} intro train/test folders".format(test_path))
-splitfolders.ratio("C:/Users/janek/__haemorrhage_project__/__datasets__/x_lrg_formatted/all", output="output",
+path = "C:/Users/janek/__haemorrhage_project__/__datasets__/x_lrg_formatted/all"
+print("Splitting folder {} intro train/test folders".format(path))
+splitfolders.ratio(path, output="output",
                   seed=1337, ratio=(.8, .2), group_prefix=None, move=False)
